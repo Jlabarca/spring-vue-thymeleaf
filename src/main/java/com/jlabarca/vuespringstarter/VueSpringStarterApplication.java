45b21e8f3345429fd9474bd3193bbe3ea8c6bb14
JLabarca <jlabarca@dparadig.com>
2018-11-13 11:35:32 -0300

    first commit

---
+++ src/main/java/com/jlabarca/vuespringstarter/VueSpringStarterApplication.java
@@ -0,0 +1,12 @@
+package com.jlabarca.vuespringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jlabarca/vuespringstarter/VueSpringStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jlabarca.vuespringstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VueSpringStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
